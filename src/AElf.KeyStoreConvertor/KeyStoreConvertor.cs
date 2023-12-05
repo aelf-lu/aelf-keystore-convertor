9df36d260af04394875f0f252fa716173c0360f7
aelf-lu <yimeng.lu@aelf.io>
2023-12-05 16:10:30 +0800

    fix: update params

--- src/AElf.KeyStoreConvertor/KeyStoreConvertor.cs
+++ src/AElf.KeyStoreConvertor/KeyStoreConvertor.cs
@@ -1,14 +1,34 @@
-using System.IO;
+using AElf.Cryptography;
 using AElf.KeyStore;
+using AElf.Types;
 
 namespace AElf.KeyStoreConvertor;
 
 public static class KeyStoreConvertor
 {
-    public static void Convert(string privateKey, string keyStorePassword, string keyStorePath)
+    public static void Convert(string privateKey, string keyStorePassword, string keyStoreDir)
     {
-        AElfKeyStoreService aelfKeyStoreService = new AElfKeyStoreService();
-        var json = aelfKeyStoreService.EncryptKeyStoreAsJson(keyStorePassword, privateKey);
-        File.WriteAllText(keyStorePath, json);
+        var aelfKeyStoreService = new AElfKeyStoreService();
+        var jsonContent = aelfKeyStoreService.EncryptKeyStoreAsJson(keyStorePassword, privateKey);
+        var keyPair = CryptoHelper.FromPrivateKey(ByteArrayHelper.HexStringToByteArray(privateKey));
+        var address = Address.FromPublicKey(keyPair.PublicKey).ToBase58();
+        var keyStorePath = Path.Combine(GetKeyStoreDir(keyStoreDir), $"{address}.json");
+        File.WriteAllText(keyStorePath, jsonContent);
     }
+
+    private static string GetKeyStoreDir(string keyStoreDir)
+    {
+        if (string.IsNullOrEmpty(keyStoreDir))
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        if (!Directory.Exists(keyStoreDir))
+        {
+            throw new KeyStoreConvertorException($"Directory {keyStoreDir} not exist.");
+        }
+
+        return keyStoreDir;
+    }
+
 }
--- src/AElf.KeyStoreConvertor/Program.cs
+++ src/AElf.KeyStoreConvertor/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.Threading.Tasks;
 
 namespace AElf.KeyStoreConvertor;
 
@@ -9,19 +8,21 @@ internal static class Program
     private static async Task<int> Main(string[] args)
     {
         // Define Root command
-        var rootCommand = new RootCommand("This is a tool for converting PrivateKey into a KeyStore");
+        var rootCommand = new RootCommand("This is a tool for converting PrivateKey into a KeyStore.");
 
         // Define argument type, name and description
-        var privateKey = new Argument<string>(name: "PrivateKey", description: "PrivateKey of convert");
-        var keyStorePassword = new Argument<string>(name: "KeyStorePassword", description: "Password of keyStore file");
-        var keyStorePath = new Argument<string>(name: "KeyStorePath", description: "Path of keyStore file");
+        var privateKey = new Argument<string>(name: "PrivateKey", description: "PrivateKey of convert.");
+        var keyStorePassword = new Argument<string>(name: "KeyStorePassword", description: "Password of keyStore file.");
         rootCommand.AddArgument(privateKey);
         rootCommand.AddArgument(keyStorePassword);
-        rootCommand.AddArgument(keyStorePath);
+
+        // Define option argument name and description
+        var keyStoreDir = new Option<string>(aliases: new[] {"--KeyStoreDir"},
+            description: "Directory of keyStore file, not include file name. When the value is null, dir is the current directory.");
+        rootCommand.AddOption(keyStoreDir);
 
         rootCommand.Handler = CommandHandler.Create<string, string, string>(KeyStoreConvertor.Convert);
 
         return await rootCommand.InvokeAsync(args);
     }
-
 }
